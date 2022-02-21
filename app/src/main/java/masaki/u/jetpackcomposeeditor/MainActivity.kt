6debb6914aea218d025b9f776ff347263c6932c4
内田正輝 <masaki.u@arsaga.jp>
2022-02-22 02:47:58 +0900

    new compose project

---
+++ app/src/main/java/masaki/u/jetpackcomposeeditor/MainActivity.kt
@@ -0,0 +1,22 @@
+package masaki.u.jetpackcomposeeditor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import masaki.u.jetpackcomposeeditor.ui.theme.JetpackComposeEditorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TODO()
+        }
+    }
+}
